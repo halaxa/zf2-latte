2c74ad523a66afdc017587b2eea26f518495c73b
Filip Halaxa <filip.halaxa@elnino.cz>
2014-05-10 18:55:34 +0200

    test/bootstrap.php fixes

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -18,14 +18,20 @@ function runRoute($route) {
     if ($route[0] !== '/') {
         $route = '/' . $route;
     }
+
     $_SERVER_OLD = $_SERVER;
     $_SERVER = require __DIR__ . '/$_SERVER.php';
+    $_SERVER['REQUEST_URI'] = $route;
+    $_SERVER['PATH_INFO'] =  $route;
+    $_SERVER['PHP_SELF'] = '/index.php'.$route;
+
     $appConfig = require __DIR__ . '/app/application.config.php';
     Zend\Console\Console::overrideIsConsole(false);
     $app = Zend\Mvc\Application::init($appConfig);
     ob_start();
     $app->run();
     $result = ob_get_clean();
+
     $_SERVER = $_SERVER_OLD;
     return $result;
 }
